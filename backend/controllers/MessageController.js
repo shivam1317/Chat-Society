0a6403e496421dadb283b847a936561cdd3f3dd6
shivam danawale <shivamdanawale@gmail.com>
2023-02-04 23:12:51 +0530

    AuthorId and MsgType changes

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -2,14 +2,16 @@ const prisma = require("../prisma/index");
 exports.sendMsg = async (messageData) => {
   try {
     console.log(
-      `author : ${messageData.author} | message : ${messageData.message} | channelId: ${messageData.channelId}`
+      `author : ${messageData.author} | message : ${messageData.message} | channelId: ${messageData.channelId} | authorId: ${messageData.authorId}`
     );
     // const { message, channelId, author } = req.body;
     const result = await prisma.message.create({
       data: {
         message: messageData.message,
         author: messageData.author,
+        authorId: messageData.authorId,
         channelName: { connect: { id: messageData.channelId } },
+        type: messageData.type,
       },
     });
     console.log(result);
@@ -28,19 +30,6 @@ exports.getMessages = async (req, res, next) => {
   try {
     const { channelId } = req.params;
     let msgs;
-    // if (parseInt(take) && !Number.isNaN(parseInt(take))) {
-    //   msgs = await prisma.message.findMany({
-    //     where: {
-    //       channelId,
-    //     },
-    //     take: parseInt(take || "10"),
-    //     skip: parseInt(take) > 10 ? parseInt(take) - 10 : undefined,
-    //     orderBy: {
-    //       timestamp: "asc",
-    //     },
-    //   });
-    //   console.log("if wala block chaala vai");
-    // } else {
     msgs = await prisma.message.findMany({
       where: {
         channelId,
@@ -49,7 +38,6 @@ exports.getMessages = async (req, res, next) => {
         timestamp: "asc",
       },
     });
-    console.log("else wala block chaala vai");
     let flag = parseInt(take) && !Number.isNaN(parseInt(take));
     const data = {
       msgs: flag ? msgs.slice(-1) : msgs,
