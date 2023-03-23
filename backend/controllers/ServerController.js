2068cfd4cc6a24d5ab683135ad21969d0a1c63b2
shivam danawale <shivamdanawale@gmail.com>
2023-03-23 18:38:53 +0530

    feat: add edit user routes

--- backend/controllers/ServerController.js
+++ backend/controllers/ServerController.js
@@ -12,33 +12,39 @@ exports.getServerList = async (req, res, next) => {
 
 exports.addServer = async (req, res, next) => {
   try {
-    const { Name, ownerId } = req.body;
-    const result = await prisma.server.create({
-      data: {
-        Name,
-        Owner: { connect: { id: ownerId } },
-        Code: uid(8),
-      },
-    });
-    let d = {
-      Name,
-      id: result.id,
-      Code: result.Code,
-    };
-    const ok = await prisma.user.update({
-      where: {
-        id: ownerId,
-      },
-      data: {
-        joinedServers: {
-          push: d,
+    const { Name, ownerId, api_secret } = req.body;
+    if (api_secret !== process.env.API_SECRET) {
+      res.status(401);
+      res.send("Unauthorized!");
+    } else {
+      const result = await prisma.server.create({
+        data: {
+          Name,
+          Owner: { connect: { id: ownerId } },
+          Code: uid(8),
         },
-      },
-    });
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.json(result);
+      });
+      let d = {
+        Name,
+        id: result.id,
+        Code: result.Code,
+        ownerId,
+      };
+      const ok = await prisma.user.update({
+        where: {
+          id: ownerId,
+        },
+        data: {
+          joinedServers: {
+            push: d,
+          },
+        },
+      });
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+      res.header("Access-Control-Allow-Headers", "Content-Type");
+      res.json(result);
+    }
   } catch (error) {
     res.json(error.message);
   }
@@ -47,36 +53,42 @@ exports.addServer = async (req, res, next) => {
 exports.joinServer = async (req, res, next) => {
   try {
     // Find the server by id
-    const { userId, Code } = req.body;
-    const servers = await prisma.server.findMany({
-      where: {
-        Code,
-      },
-    });
-    // Add this server in user's list if it exists
-    if (servers.length > 0) {
-      let d = {
-        Name: servers[0].Name,
-        id: servers[0].id,
-        Code,
-      };
-      try {
-        await prisma.user.update({
-          where: {
-            id: userId,
-          },
-          data: {
-            joinedServers: {
-              push: d,
-            },
-          },
-        });
-        res.json({ msg: "You successfully joined the home!" });
-      } catch (error) {
-        res.json(error.message);
-      }
+    const { userId, Code, api_secret } = req.body;
+    if (api_secret !== process.env.API_SECRET) {
+      res.status(401);
+      res.send("Unauthorized!");
     } else {
-      res.json({ msg: "Home Doesn't exist!" });
+      const servers = await prisma.server.findMany({
+        where: {
+          Code,
+        },
+      });
+      // Add this server in user's list if it exists
+      if (servers.length > 0) {
+        let d = {
+          Name: servers[0].Name,
+          id: servers[0].id,
+          ownerId: servers[0].ownerId,
+          Code,
+        };
+        try {
+          await prisma.user.update({
+            where: {
+              id: userId,
+            },
+            data: {
+              joinedServers: {
+                push: d,
+              },
+            },
+          });
+          res.json({ msg: "You successfully joined the home!" });
+        } catch (error) {
+          res.json(error.message);
+        }
+      } else {
+        res.json({ msg: "Home Doesn't exist!" });
+      }
     }
   } catch (error) {
     res.json(error.message);
--- backend/controllers/Usercontroller.js
+++ backend/controllers/Usercontroller.js
@@ -1,26 +1,53 @@
 const prisma = require("../prisma/index");
 exports.addUser = async (req, res, next) => {
   try {
-    const { Email, Name } = req.body;
-    const uniqueUser = await prisma.user.findMany({
-      where: {
-        Email,
-      },
-    });
-    if (uniqueUser.length === 0) {
-      const result = await prisma.user.create({
-        data: {
+    const { Email, Name, api_secret } = req.body;
+    if (api_secret !== process.env.API_SECRET) {
+      res.status(401);
+      res.json({ msg: "UnAuthorized!" });
+    } else {
+      const uniqueUser = await prisma.user.findMany({
+        where: {
           Email,
-          Name,
-          joinedServers: [],
         },
       });
-      res.json(result);
-    } else {
-      res.json(uniqueUser[0]);
+      if (uniqueUser.length === 0) {
+        const result = await prisma.user.create({
+          data: {
+            Email,
+            Name,
+            joinedServers: [],
+          },
+        });
+        res.json(result);
+      } else {
+        res.json(uniqueUser[0]);
+      }
     }
   } catch (error) {
     res.json(error.messsage);
+  }
+};
+
+exports.editUser = async (req, res, next) => {
+  try {
+    const { Name, id, api_secret } = req.body;
+    if (api_secret !== process.env.API_SECRET) {
+      res.status(401);
+      res.send("Unauthorized!");
+    } else {
+      const result = await prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          Name,
+        },
+      });
+      res.json(result);
+    }
+  } catch (error) {
+    res.json(error.message);
   }
 };
 
